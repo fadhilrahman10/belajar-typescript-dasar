622d61b406440134b1d77f0fdb1e2cb15c506217
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-12 05:58:50 +0700

    feat: data type array

---
+++ tests/array.test.ts
@@ -0,0 +1,9 @@
+describe('Array', () => {
+    it('Should return an array', () => {
+        const names: string[] = ['a', 'b', 'c'];
+        const values: number[] = [1, 2, 3, 4, 5];
+
+        console.info(names);
+        console.info(values)
+    })
+});
