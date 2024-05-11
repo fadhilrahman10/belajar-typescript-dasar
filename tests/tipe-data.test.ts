5838d6c088c63b186292953f4f2017b852b0c8bd
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-12 05:45:11 +0700

    feat: data type primitive

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', () => {
+    it('Should must declare', () => {
+        const name: string = "Fadhil Rahman";
+        const balance: number = 10;
+        const isVip: boolean = true;
+
+        console.log(name);
+        console.log(balance);
+        console.log(isVip);
+    })
+})
