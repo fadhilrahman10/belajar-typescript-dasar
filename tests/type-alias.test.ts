ab29897344da36cc8095318395d78766c32a83b7
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-12 06:28:26 +0700

    feat: data type alias

---
+++ tests/type-alias.test.ts
@@ -0,0 +1,20 @@
+import {Category, Product} from "../src/type-alias";
+
+describe('Type Alias', () => {
+    it('should support in typescript', () => {
+        const category: Category = {
+            id: "uuid",
+            name: "phone",
+        };
+
+        const product: Product = {
+            id: "uuid",
+            name: "Iphone",
+            price: 8000000,
+            category: category
+        };
+
+        console.info(category);
+        console.info(product);
+    });
+});
