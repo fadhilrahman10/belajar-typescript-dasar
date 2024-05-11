94861bc858b02a71bb92e1348cadeb99c2c6d4b8
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-12 05:53:22 +0700

    feat: babel typescript

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,11 +1,22 @@
 describe('Data Type', () => {
     it('Should must declare', () => {
-        const name: string = "Fadhil Rahman";
-        const balance: number = 10;
-        const isVip: boolean = true;
+        let name: string = "Fadhil Rahman";
+        let balance: number = 10;
+        let isVip: boolean = true;
 
         console.log(name);
         console.log(balance);
         console.log(isVip);
+
+        /*
+        * Note
+        *
+        * Error typescript kadang sering tidak terjadi di unit test,
+        * di karenakan babel secara otomatis menghapus kode typescript nya menjadi javascript.
+        *
+        * */
+        // name = 10;
+        // balance = "20";
+        // isVip = "10";
     })
 })
