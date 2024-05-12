ad03e5fef72e6bca479b31f47b2545da3508eba3
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-13 05:33:26 +0700

    feat: switch statement

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -17,4 +17,21 @@ describe('If Statement', () => {
         const say = value >= 75 ? "Congratulations" : "Try Again";
         console.info(say);
     });
+
+    it('should support switch statement', () => {
+        function sayHello(value: string): string {
+            switch (value) {
+                case "Jon":
+                    return "Hello Jon";
+                case "Doe":
+                    return "Hello Doe";
+                default :
+                    return "Hello";
+            }
+        }
+
+        console.info(sayHello("Jon"));
+        console.info(sayHello("Doe"));
+        console.info(sayHello("Lauren"));
+    });
 });
