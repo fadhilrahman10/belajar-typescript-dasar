bee6cd4030d8077150fb969aea81c2d09b19ce69
fadhilrahman <fadhilrahman920@gmail.com>
2024-05-12 07:03:22 +0700

    feat: readonly properties in interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -4,7 +4,9 @@ describe('Interface', () => {
     it('should support in typescript', () => {
         const seller: Seller = {
             id: 1,
-            name: "Seller"
+            name: "Seller",
+            nib: "123",
+            npwp: "321"
         };
 
         console.info(seller);
